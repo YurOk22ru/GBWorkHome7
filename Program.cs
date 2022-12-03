aee6f8104ce1ed5370d2c6849c749311f9299bfb
YurOk22ru <yurok-potolok@mail.ru>
2022-12-03 13:00:02 +0300

    work07

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// *Сделать вывод только нечетных коэффициентов у треугольника распечатайте хотя бы 120 строк
+
+int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++){
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++){
+    for (int j = 1; j <= i; j++){
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+
+void PrintTriangle(int[,] triangle){
+  int row = triangle.GetLength(0);
+  for (int i = 0; i < row; i++){
+    for (int j = 0; j < row; j++){
+        if (triangle[i, j] % 2 != 0)
+            Console.Write("*");
+        else{
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] tr = CreateTriangle(120); 
+PrintTriangle(tr);
